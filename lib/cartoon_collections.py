bfa174dbc47389acbde713e657ba9057f96ad579
Liam Brophy <liambrophy@Liams-MacBook-Pro.local>
2025-02-17 19:33:36 -0500

    completes lab

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -1,11 +1,20 @@
-def roll_call_dwarves():
-    pass
+def roll_call_dwarves(dwarves):
+    for index, name in enumerate(dwarves, start=1):
+        print(f"{index}. {name}")
 
-def summon_captain_planet():
-    pass
+def summon_captain_planet(palaneteer_calls):
+    summons = [call.capitalize() + "!" for call in palaneteer_calls]
+    return summons
 
-def long_planeteer_calls():
-    pass
+def long_planeteer_calls(calls):
+ for call in calls:
+    if len(call) > 4:
+        return True
+ return False
 
-def find_the_cheese():
-    pass
+def find_the_cheese(foods):
+    cheeses = ["cheddar", "gouda", "camembert"]
+    for food in foods:
+        if food in cheeses:
+            return food
+    return None
